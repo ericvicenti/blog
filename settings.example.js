f05d5e950ad4d8731ebfe388f5aab38972ae0fa7
Eric Vicenti <ericvicenti@gmail.com>
2012-11-09 19:23:00 -0800

    Update settings

--- settings.example.js
+++ settings.example.js
@@ -1,13 +1,28 @@
 // Config
 
 module.exports = {
-	host: 'localhost',
+	
+	host: 'myblogname.net',
+	
 	port: 8088,
 	securePort: 4438,
+	
 	googleID: 'YOUR GOOGLE ID',
+	
 	postmarkAPIKey: 'YOUR POSTMARK API KEY',
-	pfxPath: 'keys/localhost.pfx',
+
+	// the default key for the HTTPS server
+	pfxPath: 'keys/projectopencontent.org.pfx',
+
+	// all of the supported hosts
+	hosts: ['myblogname.net'],
+
+	// the directory where each hosts .pfx file can be found
+	keyPath: 'keys',
+
 	defaultRowLimit: 2,
+	
 	maxRowLimit: 100,
+	
 	reservedPathFirstTerms: ['login','pages','edit','preview']
 }
